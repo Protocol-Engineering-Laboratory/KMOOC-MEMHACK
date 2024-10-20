e589bb1d2ce7b41987a9c07436d82d6f5186cd36
neutrinox4b1 <neutrinox4b1@naver.com>
2024-10-20 10:39:20 -0700

    week7

---
+++ week7/7-1/stack5.c
@@ -0,0 +1,13 @@
+// gcc -o stack5 stack5.c -mpreferred-stack-boundary=4 -fno-stack-protector -no-pie -fno-pic
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	char buffer[64];
+
+	printf("Address of the buffer: %p\n", buffer);
+	gets(buffer);
+}
+
